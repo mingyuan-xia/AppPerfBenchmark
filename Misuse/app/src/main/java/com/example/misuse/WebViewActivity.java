3d10a9e7d0e179487c04b53c01605df03f213cc1
zzzkky <philip.20050132@163.com>
2016-01-05 12:35:06 +0800

    change layout

--- Misuse/app/src/main/java/com/example/misuse/WebViewActivity.java
+++ Misuse/app/src/main/java/com/example/misuse/WebViewActivity.java
@@ -9,6 +9,8 @@ import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -16,12 +18,15 @@ import java.net.URL;
 
 public class WebViewActivity extends Activity {
 private WebView webview;
+private EditText tv;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_webview);
 
         webview = (WebView)findViewById(R.id.webview);
+        tv = (EditText)findViewById(R.id.WebView_tv);
+        Button enter = (Button)findViewById(R.id.WebView_enter);
         Button bt1 = (Button)findViewById(R.id.WebView_bt1);
         Button bt2 = (Button)findViewById(R.id.WebView_bt2);
         Button bt3 = (Button)findViewById(R.id.WebView_bt3);
@@ -32,26 +37,28 @@ private WebView webview;
         bt1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Simplest usage: note that an exception will NOT be thrown
-                // if there is an error loading this page (see below).
-                webview.loadUrl("http://www.baidu.com/");
+                // OR, you can also load from an HTML string:
+                String summary = "<html>" +
+                        "<body>You scored <b>192</b> points.</body>" +
+                        "<img src=\"https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo_top_ca79a146.png\"></img>" +
+                        "</html>";
+                webview.loadData(summary, "text/html", null);
+                // ... although note that there are restrictions on what this HTML can do.
+                // See the JavaDocs for loadData() and loadDataWithBaseURL() for more info.
+
             }
         });
         bt2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // OR, you can also load from an HTML string:
-                String summary = "<html><body>You scored <b>192</b> points.</body></html>";
-                webview.loadData(summary, "text/html", null);
-                // ... although note that there are restrictions on what this HTML can do.
-                // See the JavaDocs for loadData() and loadDataWithBaseURL() for more info.
+
+                webview.getSettings().setJavaScriptEnabled(false);
+                webview.loadUrl(tv.getText().toString().trim());
             }
         });
         bt3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Let's display the progress in the activity title bar, like the
-                // browser app does.
                 webview.getSettings().setJavaScriptEnabled(true);
 
                 final Activity activity = WebViewActivity.this;
@@ -61,8 +68,15 @@ private WebView webview;
                         Toast.makeText(activity, "Oh no! " + description, Toast.LENGTH_SHORT).show();
                     }
                 });
-                
-                webview.loadUrl("http://www.baidu.com/");
+
+                webview.loadUrl(tv.getText().toString().trim());
+            }
+        });
+
+        enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                webview.loadUrl(tv.getText().toString().trim());
             }
         });
     }
